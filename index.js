a3aa073d2f48a51ef94575ffd513486a41299175
DuMark Allman <dumark.allman@yahoo.com>
2023-03-28 19:33:56 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(records) {
+const foundObject = records.find(record => record.result === "W")
+if (foundObject) { //to return the year of the object 
+return foundObject.year
+}
+}
